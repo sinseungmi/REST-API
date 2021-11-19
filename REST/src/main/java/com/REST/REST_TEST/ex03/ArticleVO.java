bf141cab9ae4512c53beb7ab203575d967ea524e
sinseungmi <tlstmdal3@naver.com>
2021-11-19 22:47:02 +0900

    ArticleVO 데이터 저장

--- REST/src/main/java/com/REST/REST_TEST/ex03/ArticleVO.java
+++ REST/src/main/java/com/REST/REST_TEST/ex03/ArticleVO.java
@@ -1,5 +1,13 @@
 package com.REST.REST_TEST.ex03;
 
+import lombok.Data;
+
+@Data
 public class ArticleVO {
+	
+	private int articleNO;
+	private String writer;
+	private String title;
+	private String content;
 
 }
--- REST/src/main/java/com/REST/REST_TEST/ex03/BoardController.java
+++ REST/src/main/java/com/REST/REST_TEST/ex03/BoardController.java
@@ -1,15 +1,35 @@
 package com.REST.REST_TEST.ex03;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j;
 
+@RestController
+@RequestMapping("/boards")
 @Log4j
 public class BoardController {
+									//get방식으로 요청하기 때문에 모든 글의 정보를 조회
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
 	public ResponseEntity<List<ArticleVO>> listArticles(){
+		log.info("list 메서드 호출");
+		List<ArticleVO> list = new ArrayList<ArticleVO>();
 		
+		for (int i = 0; i < 10; i++) {
+			ArticleVO vo = new ArticleVO();
+			vo.setArticleNO(i);
+			vo.setWriter("이순신" + i);
+			vo.setTitle("안녕하세요" + i);
+			vo.setContent("새 상품을 소개합니다." + i);
+			list.add(vo);
+		}
+		return new ResponseEntity<List<ArticleVO>>(list, HttpStatus.OK);
 	}
 
 }
